5b4ddc16ba95ed2ebeb04db12d685dc042dc04d7
Michael Schmidt <michael.j.schmidt@gmail.com>
2014-08-26 23:44:35 +0200

    init

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+register_asset "stylesheets/phalanx.scss"
+
+register_color_scheme 'phalanx', {
+  primary:           "ffffff",
+  secondary:         "151714",
+  tertiary:          "999900",
+  quaternary:        "722000",
+  header_background: "000000",
+  header_primary:    "ffffff",
+  highlight:         "33ff33",
+  danger:            "e45735",
+  success:           "009900",
+  love:              "fa6c8d"
+}
